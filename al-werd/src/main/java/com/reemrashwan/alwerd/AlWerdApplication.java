077c61c59b73abb9e0499795bd294eb428467edf
Reem Khaled <roooka219@gmail.com>
2022-10-20 22:57:50 +0200

    Initial maven files

---
+++ al-werd/src/main/java/com/reemrashwan/alwerd/AlWerdApplication.java
@@ -0,0 +1,13 @@
+package com.reemrashwan.alwerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlWerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlWerdApplication.class, args);
+	}
+
+}
---
+++ al-werd/src/test/java/com/reemrashwan/alwerd/AlWerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reemrashwan.alwerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlWerdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
